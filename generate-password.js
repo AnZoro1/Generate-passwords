f1fc2d777577a72db81e26251607b773056d946b
AnzorKarto <beit06@mail.ru>
2022-11-05 20:18:24 +0300

    Сделал все подзаддачи, чтобы пароли не совпадали в массивах вызвал саму функцию внутри себя - замыкание

--- generate-password.js
+++ generate-password.js
@@ -1,8 +1,16 @@
-function generatePassword(length, level) {
+function generatePassword(length, level, amount) {
   const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
   let newPassword = ''
   const digits = '0123456789'
   const symbols = '!#$%&()*+,-./:;<=>?@[]^_{|}~'
+  let array = []
+  if (amount) {
+    for (let z = 0; z < amount; z++) {
+      array.push(generatePassword(length, level))
+    }
+    return array
+  }
+
   if (level == 'easy') {
     for (let i = 0; i < length; i++) {
       const randomSymbol = alphabet[Math.floor(Math.random() * alphabet.length)]
@@ -20,7 +28,9 @@ function generatePassword(length, level) {
     }
     let arr = newPassword.split('')
     arr.length = length
-    return arr.join('')
+    newPassword = arr.join('')
+
+    return newPassword
   }
   if (level == 'hard') {
     for (let k = 0; k < length; k++) {
@@ -32,8 +42,10 @@ function generatePassword(length, level) {
     }
     let arr1 = newPassword.split('')
     arr1.length = length
-    return arr1.join('')
+    newPassword = arr1.join('')
+
+    return newPassword
   }
 }
 
-console.log(generatePassword(5, 'easy'))
+console.log(generatePassword(5, 'hard'))
